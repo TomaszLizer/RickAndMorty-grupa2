a4fdcb63e75f9f565fc28a65140a1c27ff51c2bf
TomaszLizer <tomasz.lizer@anywhere.co>
2024-10-25 08:51:44 +0200

    Create CharacterList View

---
+++ RickAndMorty-grupa2/Views/CharacterList/CharacterList.swift
@@ -0,0 +1,15 @@
+//
+//  CharacterList.swift
+//  RickAndMorty-grupa2
+//
+//  Created by Tomasz Lizer on 25/10/2024.
+//
+
+import SwiftUI
+
+struct CharacterList: View {
+    
+    var body: some View {
+        Text("CharacterList")
+    }
+}
