2fc974d9bae9fc8a98e4bd25f34674bf066985e4
TomaszLizer <tomasz.lizer@anywhere.co>
2024-10-25 07:32:36 +0200

    Initial Commit

---
+++ RickAndMorty-grupa2/RickAndMorty_grupa2App.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMorty_grupa2App.swift
+//  RickAndMorty-grupa2
+//
+//  Created by Tomasz Lizer on 25/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMorty_grupa2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RickAndMorty-grupa2Tests/RickAndMorty_grupa2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMorty_grupa2Tests.swift
+//  RickAndMorty-grupa2Tests
+//
+//  Created by Tomasz Lizer on 25/10/2024.
+//
+
+import Testing
+@testable import RickAndMorty_grupa2
+
+struct RickAndMorty_grupa2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
