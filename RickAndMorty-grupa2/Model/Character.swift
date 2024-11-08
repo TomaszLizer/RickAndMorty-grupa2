16ffa8d0745732007a31a899f0e7588e76788e9f
TomaszLizer <tomasz.lizer@anywhere.co>
2024-11-08 09:41:41 +0100

    Extend Gender model with custom properties

--- RickAndMorty-grupa2/Model/Character.swift
+++ RickAndMorty-grupa2/Model/Character.swift
@@ -37,4 +37,31 @@ enum Gender: String, Decodable{
     case female = "Female"
     case unknown
     case genderless = "Genderless"
+    
+    var displayTitle: String {
+        switch self {
+        case .male: return "Male"
+        case .female: return "Female"
+        case .unknown: return "Unknown"
+        case .genderless: return "Genderless"
+        }
+    }
+    
+    var symbolName: String {
+        switch self {
+        case .male: return "mustache.fill"
+        case .female: return "mouth.fill"
+        case .unknown: return "questionmark.folder"
+        case .genderless: return "brain.fill"
+        }
+    }
+    
+    var symbolColor: Color {
+        switch self {
+        case .male: return .brown
+        case .female: return .red
+        case .unknown: return .gray
+        case .genderless: return .pink
+        }
+    }
 }
