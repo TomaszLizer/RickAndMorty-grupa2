fc88e5a201fc874d7677197103d9d6fea41ae692
TomaszLizer <tomasz.lizer@anywhere.co>
2024-11-08 09:03:43 +0100

    Debugging layout with colors

--- RickAndMorty-grupa2/Views/CharacterList/CharacterListRow.swift
+++ RickAndMorty-grupa2/Views/CharacterList/CharacterListRow.swift
@@ -11,10 +11,16 @@ struct CharacterListRow: View {
     var character: Character
     
     var body: some View {
-        VStack {
+        VStack(
+            alignment: .leading,
+            spacing: 0
+        ) {
             Text(character.name)
+                .background(Color.green)
             Text(character.gender.rawValue)
+                .background(Color.yellow)
         }
+        .background(Color.red)
     }
 }
 
