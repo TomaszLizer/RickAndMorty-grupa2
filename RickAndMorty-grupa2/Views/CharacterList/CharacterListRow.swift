6029bae21a78a2d4e0935df8ccda7b1fb113dbaa
TomaszLizer <tomasz.lizer@anywhere.co>
2024-11-22 07:45:28 +0100

    Add Image to the CharacterListRow
    Why so big?

--- RickAndMorty-grupa2/Views/CharacterList/CharacterListRow.swift
+++ RickAndMorty-grupa2/Views/CharacterList/CharacterListRow.swift
@@ -10,16 +10,30 @@ import SwiftUI
 struct CharacterListRow: View {
     var character: Character
     
+    @State private var image: UIImage?
+    
     var body: some View {
-        VStack(
-            alignment: .leading,
-            spacing: 0
-        ) {
-            Text(character.name)
-                .font(.title)
-            let gender = character.gender
-            Image(systemName: gender.symbolName)
-                .foregroundStyle(gender.symbolColor)
+        HStack(alignment: .center) {
+            Image(uiImage: image ?? UIImage())
+            VStack(
+                alignment: .leading,
+                spacing: 0
+            ) {
+                Text(character.name)
+                    .font(.title)
+                let gender = character.gender
+                Image(systemName: gender.symbolName)
+                    .foregroundStyle(gender.symbolColor)
+            }
+        }
+        .task {
+            let session = URLSession.shared
+            do {
+                let (imageData, _) = try await session.data(from: character.image)
+                image = UIImage(data: imageData)
+            } catch {
+                // handle error
+            }
         }
     }
 }
